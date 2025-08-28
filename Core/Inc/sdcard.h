0fffa22900d81468dda439cede053c8267e69935
Harshil <37377066+harshil21@users.noreply.github.com>
2025-08-28 01:02:30 -0400

    Improve sd card init/append/close

--- Core/Inc/sdcard.h
+++ Core/Inc/sdcard.h
@@ -14,14 +14,33 @@
 
 #include <string.h>
 
-FRESULT AppendToFile(char* path, size_t path_len, const char* msg, size_t msg_len);
+/**
+ * @brief Initialize the SD card. Initialize the filesystem, mount it, and open the file and return the file object.
+ * @param path: The path to the file we want to write.
+ * @retval return the file object
+ */
+FRESULT sdCardInit(FIL *file_obj, char *path, size_t path_len);
+
+/**
+ * @brief adds data to file
+ */
+FRESULT AppendToFile(FIL *file_obj, const char *msg, size_t msg_len);
+
+/**
+ * @brief Save the file contents instantly
+ * @param file_obj: Pointer to the file object from sdCardInit
+ */
+void sdCardSave(FIL *file_obj);
+
+/**
+ * @brief Close the file and sync it.
+ * @param file_obj: the file object returned after initializing the sd card
+ */
+void sdCardClose(FIL *file_obj);
 
 extern FRESULT fres;
 extern uint16_t raw_temp;
 extern char log_path[];
 extern char buf[];
-
-FRESULT sdcard_init(void);
-FRESULT sdcard_write(const char* temp);
 
 #endif /* INC_SDCARD_H_ */
