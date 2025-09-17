2e6464fd210fce4f6efa5eb28918b20a3afc4093
Graicc <33105645+Graicc@users.noreply.github.com>
2025-09-17 13:25:02 -0400

    Copy over FATFS from DMA version

--- FATFS/Target/ffconf.h
+++ FATFS/Target/ffconf.h
@@ -53,7 +53,7 @@
 /  1: Enable without LF-CRLF conversion.
 /  2: Enable with LF-CRLF conversion. */
 
-#define _USE_FIND            0
+#define _USE_FIND            1
 /* This option switches filtered directory read functions, f_findfirst() and
 /  f_findnext(). (0:Disable, 1:Enable 2:Enable with matching altname[] too) */
 
@@ -63,7 +63,7 @@
 #define _USE_FASTSEEK        1
 /* This option switches fast seek feature. (0:Disable or 1:Enable) */
 
-#define	_USE_EXPAND		0
+#define	_USE_EXPAND		1
 /* This option switches f_expand function. (0:Disable or 1:Enable) */
 
 #define _USE_CHMOD		0
@@ -109,7 +109,7 @@
 /   950 - Traditional Chinese (DBCS)
 */
 
-#define _USE_LFN     0    /* 0 to 3 */
+#define _USE_LFN     1    /* 0 to 3 */
 #define _MAX_LFN     255  /* Maximum LFN length to handle (12 to 255) */
 /* The _USE_LFN switches the support of long file name (LFN).
 /
@@ -175,7 +175,7 @@
 /  arbitrary physical drive and partition listed in the VolToPart[]. Also f_fdisk()
 /  function will be available. */
 #define _MIN_SS    512  /* 512, 1024, 2048 or 4096 */
-#define _MAX_SS    512  /* 512, 1024, 2048 or 4096 */
+#define _MAX_SS    4096  /* 512, 1024, 2048 or 4096 */
 /* These options configure the range of sector size to be supported. (512, 1024,
 /  2048 or 4096) Always set both 512 for most systems, all type of memory cards and
 /  harddisk. But a larger value may be required for on-board flash memory and some
@@ -209,7 +209,7 @@
 /  Instead of private sector buffer eliminated from the file object, common sector
 /  buffer in the file system object (FATFS) is used for the file data transfer. */
 
-#define _FS_EXFAT	0
+#define _FS_EXFAT	1
 /* This option switches support of exFAT file system. (0:Disable or 1:Enable)
 /  When enable exFAT, also LFN needs to be enabled. (_USE_LFN >= 1)
 /  Note that enabling exFAT discards C89 compatibility. */
@@ -227,7 +227,7 @@
 /  _NORTC_MDAY and _NORTC_YEAR have no effect.
 /  These options have no effect at read-only configuration (_FS_READONLY = 1). */
 
-#define _FS_LOCK    2     /* 0:Disable or >=1:Enable */
+#define _FS_LOCK    1     /* 0:Disable or >=1:Enable */
 /* The option _FS_LOCK switches file lock function to control duplicated file open
 /  and illegal operation to open objects. This option must be 0 when _FS_READONLY
 /  is 1.
@@ -239,7 +239,7 @@
 /      lock control is independent of re-entrancy. */
 
 #define _FS_REENTRANT    0  /* 0:Disable or 1:Enable */
-#define _FS_TIMEOUT      1000 /* Timeout period in unit of time ticks */
+#define _FS_TIMEOUT      1 /* Timeout period in unit of time ticks */
 #define _SYNC_t          NULL
 /* The option _FS_REENTRANT switches the re-entrancy (thread safe) of the FatFs
 /  module itself. Note that regardless of this option, file access to different
--- FATFS/Target/sd_diskio.c
+++ FATFS/Target/sd_diskio.c
@@ -50,6 +50,7 @@
  */
 /* USER CODE BEGIN disableSDInit */
 /* #define DISABLE_SD_INIT */
+uint32_t sd_FastWriteFlag = 0;
 /* USER CODE END disableSDInit */
 
 /*
@@ -297,6 +298,70 @@ DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
 
 /* USER CODE BEGIN beforeWriteSection */
 /* can be used to modify previous code / undefine following code / add new code */
+DRESULT SD_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
+{
+	  DRESULT res = RES_ERROR;
+	  uint32_t timeout;
+
+	  if(sd_FastWriteFlag == 0)
+	  {
+
+	   WriteStatus = 0;
+
+	  if (SD_CheckStatusWithTimeout(SD_TIMEOUT) < 0)
+	  {
+	    return res;
+	  }
+		    if(BSP_SD_WriteBlocks_DMA((uint32_t*)buff,
+		                              (uint32_t)(sector),
+		                              count) == MSD_OK)
+		    {
+		      /* Wait that writing process is completed or a timeout occurs */
+
+		      timeout = HAL_GetTick();
+		      while((WriteStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
+		      {
+		      }
+		      /* in case of a timeout return error */
+		      if (WriteStatus == 0)
+		      {
+		        res = RES_ERROR;
+		      }
+		      else
+		      {
+		        WriteStatus = 1;
+		        timeout = HAL_GetTick();
+
+		        while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+		        {
+		          if (BSP_SD_GetCardState() == SD_TRANSFER_OK)
+		          {
+		            res = RES_OK;
+		            break;
+		          }
+		        }
+		      }
+		    }
+		  return res;
+	  }
+	  else
+	  {
+		  if (WriteStatus == 0)
+		  {
+			  res = RES_ERROR;
+		  }
+		  else
+		  {
+			  if(BSP_SD_WriteBlocks_DMA((uint32_t*)buff, (uint32_t)(sector),count) == MSD_OK)
+			  {
+				  WriteStatus = 0;
+				  res = RES_OK;
+			  }
+		  }
+		  return res;
+	  }
+}
+#if 0
 /* USER CODE END beforeWriteSection */
 /**
   * @brief  Writes Sector(s)
@@ -310,113 +375,72 @@ DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
 
 DRESULT SD_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
 {
-  DRESULT res = RES_ERROR;
-  uint32_t timeout;
-#if defined(ENABLE_SCRATCH_BUFFER)
-  uint8_t ret;
-  int i;
-#endif
+	  DRESULT res = RES_ERROR;
+	  uint32_t timeout;
 
-   WriteStatus = 0;
-#if (ENABLE_SD_DMA_CACHE_MAINTENANCE == 1)
-  uint32_t alignedAddr;
-#endif
+	  if(sd_FastWriteFlag == 0)
+	  {
 
-  if (SD_CheckStatusWithTimeout(SD_TIMEOUT) < 0)
-  {
-    return res;
-  }
+	   WriteStatus = 0;
 
-#if defined(ENABLE_SCRATCH_BUFFER)
-  if (!((uint32_t)buff & 0x3))
-  {
-#endif
-#if (ENABLE_SD_DMA_CACHE_MAINTENANCE == 1)
+	  if (SD_CheckStatusWithTimeout(SD_TIMEOUT) < 0)
+	  {
+	    return res;
+	  }
+		    if(BSP_SD_WriteBlocks_DMA((uint32_t*)buff,
+		                              (uint32_t)(sector),
+		                              count) == MSD_OK)
+		    {
+		      /* Wait that writing process is completed or a timeout occurs */
 
-    /*
-    the SCB_CleanDCache_by_Addr() requires a 32-Byte aligned address
-    adjust the address and the D-Cache size to clean accordingly.
-    */
-    alignedAddr = (uint32_t)buff &  ~0x1F;
-    SCB_CleanDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
-#endif
+		      timeout = HAL_GetTick();
+		      while((WriteStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
+		      {
+		      }
+		      /* in case of a timeout return error */
+		      if (WriteStatus == 0)
+		      {
+		        res = RES_ERROR;
+		      }
+		      else
+		      {
+		        WriteStatus = 1;
+		        timeout = HAL_GetTick();
 
-    if(BSP_SD_WriteBlocks_DMA((uint32_t*)buff,
-                              (uint32_t)(sector),
-                              count) == MSD_OK)
-    {
-      /* Wait that writing process is completed or a timeout occurs */
-
-      timeout = HAL_GetTick();
-      while((WriteStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
-      {
-      }
-      /* in case of a timeout return error */
-      if (WriteStatus == 0)
-      {
-        res = RES_ERROR;
-      }
-      else
-      {
-        WriteStatus = 0;
-        timeout = HAL_GetTick();
-
-        while((HAL_GetTick() - timeout) < SD_TIMEOUT)
-        {
-          if (BSP_SD_GetCardState() == SD_TRANSFER_OK)
-          {
-            res = RES_OK;
-            break;
-          }
-        }
-      }
-    }
-#if defined(ENABLE_SCRATCH_BUFFER)
-  }
-    else
-    {
-      /* Slow path, fetch each sector a part and memcpy to destination buffer */
-#if (ENABLE_SD_DMA_CACHE_MAINTENANCE == 1)
-      /*
-      * invalidate the scratch buffer before the next write to get the actual data instead of the cached one
-      */
-      SCB_InvalidateDCache_by_Addr((uint32_t*)scratch, BLOCKSIZE);
-#endif
-
-      for (i = 0; i < count; i++)
-      {
-        WriteStatus = 0;
-
-        memcpy((void *)scratch, (void *)buff, BLOCKSIZE);
-        buff += BLOCKSIZE;
-
-        ret = BSP_SD_WriteBlocks_DMA((uint32_t*)scratch, (uint32_t)sector++, 1);
-        if (ret == MSD_OK) {
-          /* wait for a message from the queue or a timeout */
-          timeout = HAL_GetTick();
-          while((WriteStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
-          {
-          }
-          if (WriteStatus == 0)
-          {
-            break;
-          }
-
-        }
-        else
-        {
-          break;
-        }
-      }
-      if ((i == count) && (ret == MSD_OK))
-        res = RES_OK;
-    }
-#endif
-  return res;
+		        while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+		        {
+		          if (BSP_SD_GetCardState() == SD_TRANSFER_OK)
+		          {
+		            res = RES_OK;
+		            break;
+		          }
+		        }
+		      }
+		    }
+		  return res;
+	  }
+	  else
+	  {
+		  if (WriteStatus == 0)
+		  {
+			  res = RES_ERROR;
+		  }
+		  else
+		  {
+			  if(BSP_SD_WriteBlocks_DMA((uint32_t*)buff, (uint32_t)(sector),count) == MSD_OK)
+			  {
+				  WriteStatus = 0;
+				  res = RES_OK;
+			  }
+		  }
+		  return res;
+	  }
 }
+
 #endif /* _USE_WRITE == 1 */
 
 /* USER CODE BEGIN beforeIoctlSection */
+#endif
 /* can be used to modify previous code / undefine following code / add new code */
 /* USER CODE END beforeIoctlSection */
 /**
