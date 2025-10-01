ebd941ee7c550b33e995ff60953fc50e896f5845
Will Sanderson <wlsanderson5@gmail.com>
2025-09-30 22:19:53 -0400

    fixed stm32 code

--- STM32/Debug/Core/Src/subdir.mk
+++ STM32/Debug/Core/Src/subdir.mk
@@ -5,8 +5,9 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/Src/bmp581.c \
+../Core/Src/bmp581_spi.c \
 ../Core/Src/firm_utils.c \
+../Core/Src/i2c_utils.c \
 ../Core/Src/icm45686.c \
 ../Core/Src/logger.c \
 ../Core/Src/main.c \
@@ -20,8 +21,9 @@ C_SRCS += \
 ../Core/Src/usb_print_debug.c 
 
 OBJS += \
-./Core/Src/bmp581.o \
+./Core/Src/bmp581_spi.o \
 ./Core/Src/firm_utils.o \
+./Core/Src/i2c_utils.o \
 ./Core/Src/icm45686.o \
 ./Core/Src/logger.o \
 ./Core/Src/main.o \
@@ -35,8 +37,9 @@ OBJS += \
 ./Core/Src/usb_print_debug.o 
 
 C_DEPS += \
-./Core/Src/bmp581.d \
+./Core/Src/bmp581_spi.d \
 ./Core/Src/firm_utils.d \
+./Core/Src/i2c_utils.d \
 ./Core/Src/icm45686.d \
 ./Core/Src/logger.d \
 ./Core/Src/main.d \
@@ -57,7 +60,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/bmp581.cyclo ./Core/Src/bmp581.d ./Core/Src/bmp581.o ./Core/Src/bmp581.su ./Core/Src/firm_utils.cyclo ./Core/Src/firm_utils.d ./Core/Src/firm_utils.o ./Core/Src/firm_utils.su ./Core/Src/icm45686.cyclo ./Core/Src/icm45686.d ./Core/Src/icm45686.o ./Core/Src/icm45686.su ./Core/Src/logger.cyclo ./Core/Src/logger.d ./Core/Src/logger.o ./Core/Src/logger.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/mmc5983ma.cyclo ./Core/Src/mmc5983ma.d ./Core/Src/mmc5983ma.o ./Core/Src/mmc5983ma.su ./Core/Src/spi_utils.cyclo ./Core/Src/spi_utils.d ./Core/Src/spi_utils.o ./Core/Src/spi_utils.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/usb_print_debug.cyclo ./Core/Src/usb_print_debug.d ./Core/Src/usb_print_debug.o ./Core/Src/usb_print_debug.su
+	-$(RM) ./Core/Src/bmp581_spi.cyclo ./Core/Src/bmp581_spi.d ./Core/Src/bmp581_spi.o ./Core/Src/bmp581_spi.su ./Core/Src/firm_utils.cyclo ./Core/Src/firm_utils.d ./Core/Src/firm_utils.o ./Core/Src/firm_utils.su ./Core/Src/i2c_utils.cyclo ./Core/Src/i2c_utils.d ./Core/Src/i2c_utils.o ./Core/Src/i2c_utils.su ./Core/Src/icm45686.cyclo ./Core/Src/icm45686.d ./Core/Src/icm45686.o ./Core/Src/icm45686.su ./Core/Src/logger.cyclo ./Core/Src/logger.d ./Core/Src/logger.o ./Core/Src/logger.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/mmc5983ma.cyclo ./Core/Src/mmc5983ma.d ./Core/Src/mmc5983ma.o ./Core/Src/mmc5983ma.su ./Core/Src/spi_utils.cyclo ./Core/Src/spi_utils.d ./Core/Src/spi_utils.o ./Core/Src/spi_utils.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/usb_print_debug.cyclo ./Core/Src/usb_print_debug.d ./Core/Src/usb_print_debug.o ./Core/Src/usb_print_debug.su
 
 .PHONY: clean-Core-2f-Src
 
