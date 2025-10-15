b266e50a71c33f0d8ac8af561099135e4a9e2573
sigt_175 <83317160+SR5512@users.noreply.github.com>
2025-10-15 19:10:29 -0400

    BMP and Mag Floats
    32-bit floats using given fields

--- STM32/Core/Src/preprocessor.c
+++ STM32/Core/Src/preprocessor.c
@@ -1,3 +1,23 @@
 #include "bmp581.h"
+#include "mmc5983ma.h"
 
-float temp_Comb = 
+void bmp581_convert_packet(BMP581Packet_t packet){
+    int32_t temp_binary, pressure_binary;
+    temp_binary = ((int32_t) packet.temp_msb << 16 )| ((int32_t) packet.temp_lsb << 8) | ((int32_t) packet.temp_xlsb);
+
+    pressure_binary = ((int32_t) packet.pressure_msb << 16) | ((int32_t) packet.pressure_lsb << 8) | ( (int32_t) packet.pressure_xlsb);
+
+    float temp_float = (float) temp_float/65536.0F;
+
+    float pressure_float = (float) pressure_float/64.0F;
+}
+
+void mmc5983_convert_package(MMC5983MAPacket_t packet){
+
+    int32_t mag_binary_x,mag_binary_y,mag_binary_z;
+
+    mag_binary_x = ((int32_t) packet.mag_x_msb << 10) | ((int32_t) packet.mag_x_mid << 2) | ((int32_t)(packet.mag_xyz_lsb >>6));
+    mag_binary_y = ((int32_t) packet.mag_y_msb << 10 )| ((int32_t) packet.mag_y_mid << 2) | ((int32_t)((packet.mag_xyz_lsb & 00110000)>>4));
+    mag_binary_z = ((int32_t) packet.mag_z_msb << 10 )| ((int32_t) packet.mag_z_mid << 2) | ((int32_t)((packet.mag_xyz_lsb & 00001100>>2)));
+
+}
