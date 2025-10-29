9ec624988125cbf7bc6097d164b5ae1e0fa75037
Harshil <37377066+harshil21@users.noreply.github.com>
2025-10-29 00:07:35 -0400

    Prepare for uploading to pypi

--- python/firm/__init__.py
+++ python/firm/__init__.py
@@ -1,9 +1,9 @@
 """All public classes for FIRM."""
 
 __all__ = (
-    "FIRMPacket",
     "FIRM",
+    "FIRMPacket",
 )
 
-from .packets import FIRMPacket
 from .firm import FIRM
+from .packets import FIRMPacket
