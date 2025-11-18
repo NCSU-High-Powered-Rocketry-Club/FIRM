7e3d2d19762c6538e194a36b6f383b6306b1ac90
Will Sanderson <wlsanderson5@gmail.com>
2025-11-18 12:10:32 -0500

    fix include

--- STM32/Core/Src/matrixhelper.c
+++ STM32/Core/Src/matrixhelper.c
@@ -1,10 +1,10 @@
-#include <matrix_functions.h>
+#include <arm_math.h>
 
 void symmetric(arm_matrix_instance_f32 *enter_matrix){
     uint16_t mat_rows = enter_matrix->numRows;
     uint16_t mat_cols = enter_matrix->numCols;
     uint16_t indicator = 1;
-    float32_t scale_val = .5
+    float32_t scale_val = .5;
     arm_matrix_instance_f32 output_matrix, *ptr_output_matrix;
     ptr_output_matrix = &output_matrix;
 
@@ -13,8 +13,8 @@ void symmetric(arm_matrix_instance_f32 *enter_matrix){
     }
 
     if (indicator){
-        arm_mat_trans_f32(*enter_matrix, *ptr_output_matrix);
-        arm_mat_add_f32(*enter_matrix, *ptr_output_matrix, *ptr_output_matrix);
+        arm_mat_trans_f32(enter_matrix, *ptr_output_matrix);
+        arm_mat_add_f32(enter_matrix, *ptr_output_matrix, *ptr_output_matrix);
         arm_mat_scale_f32 (*ptr_output_matrix, scale_val, *ptr_output_matrix);
     }
     
